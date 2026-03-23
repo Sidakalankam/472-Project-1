bae20d3e362f990672f5274423cabc02834b8c6f
Sid Akalankam <Sidakalankam@gmail.com>
2026-03-23 17:35:30 -0400

    Implement ray intersection polygon and matrix stack setup

--- CMyRaytraceRenderer.cpp
+++ CMyRaytraceRenderer.cpp
@@ -7,6 +7,97 @@ CMyRaytraceRenderer::CMyRaytraceRenderer()
     m_rayimagewidth = 0;
     m_rayimageheight = 0;
     m_window = NULL;
+    m_material = NULL;
+}
+
+bool CMyRaytraceRenderer::RendererStart()
+{
+    m_intersection.Initialize();
+    m_mstack.clear();
+
+    // We have to do all of the matrix work ourselves.
+    // Set up the matrix stack.
+    CGrTransform t;
+    t.SetLookAt(Eye().X(), Eye().Y(), Eye().Z(),
+                Center().X(), Center().Y(), Center().Z(),
+                Up().X(), Up().Y(), Up().Z());
+
+    m_mstack.push_back(t);
+    m_material = NULL;
+
+    return true;
+}
+
+//
+// Name : CMyRaytraceRenderer::RendererEndPolygon()
+// Description : End definition of a polygon. The superclass has
+// already collected the polygon information
+//
+void CMyRaytraceRenderer::RendererEndPolygon()
+{
+    const std::list<CGrPoint>& vertices = PolyVertices();
+    const std::list<CGrPoint>& normals = PolyNormals();
+    const std::list<CGrPoint>& tvertices = PolyTexVertices();
+
+    // Allocate a new polygon in the ray intersection system
+    m_intersection.PolygonBegin();
+    m_intersection.Material(m_material);
+
+    if (PolyTexture())
+    {
+        m_intersection.Texture(PolyTexture());
+    }
+
+    std::list<CGrPoint>::const_iterator normal = normals.begin();
+    std::list<CGrPoint>::const_iterator tvertex = tvertices.begin();
+
+    for (std::list<CGrPoint>::const_iterator i = vertices.begin(); i != vertices.end(); i++)
+    {
+        if (normal != normals.end())
+        {
+            m_intersection.Normal(m_mstack.back() * *normal);
+            normal++;
+        }
+
+        if (tvertex != tvertices.end())
+        {
+            m_intersection.TexVertex(*tvertex);
+            tvertex++;
+        }
+
+        m_intersection.Vertex(m_mstack.back() * *i);
+    }
+
+    m_intersection.PolygonEnd();
+}
+
+void CMyRaytraceRenderer::RendererMaterial(CGrMaterial* p_material)
+{
+    m_material = p_material;
+}
+
+void CMyRaytraceRenderer::RendererPushMatrix()
+{
+    m_mstack.push_back(m_mstack.back());
+}
+
+void CMyRaytraceRenderer::RendererPopMatrix()
+{
+    m_mstack.pop_back();
+}
+
+void CMyRaytraceRenderer::RendererRotate(double angle, double x, double y, double z)
+{
+    CGrTransform r;
+    r.SetRotate(angle, CGrPoint(x, y, z));
+    m_mstack.back() *= r;
+}
+
+void CMyRaytraceRenderer::RendererTranslate(double x, double y, double z)
+{
+    CGrTransform t;
+    t.SetTranslate(x, y, z);
+    m_mstack.back() *= t;
 }
 
 void CMyRaytraceRenderer::SetImage(BYTE** image, int width, int height)
--- CMyRaytraceRenderer.h
+++ CMyRaytraceRenderer.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include <list>
 #include "graphics/GrRenderer.h"
+#include "graphics/GrTransform.h"
+#include "graphics/RayIntersection.h"
 
 class CMyRaytraceRenderer : public CGrRenderer
 {
 public:
     CMyRaytraceRenderer();
+    virtual bool RendererStart();
+    virtual void RendererEndPolygon();
+    virtual void RendererMaterial(CGrMaterial* p_material);
+    virtual void RendererPushMatrix();
+    virtual void RendererPopMatrix();
+    virtual void RendererRotate(double angle, double x, double y, double z);
+    virtual void RendererTranslate(double x, double y, double z);
 
     // Set image buffer
     void SetImage(BYTE** image, int width, int height);
@@ -19,4 +29,7 @@ private:
     BYTE** m_rayimage;
 
     CWnd* m_window;
+    CRayIntersection m_intersection;
+    std::list<CGrTransform> m_mstack;
+    CGrMaterial* m_material;
 };
